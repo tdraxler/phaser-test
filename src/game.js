698e8e28b4efc3d4d3fbb056e063c9e9d9bd35bf
tdraxler <twdrax@gmail.com>
2021-04-02 17:52:42 -0700

    Added simple bouncing title demo

--- src/game.js
+++ src/game.js
@@ -1,1 +1,54 @@
+import Phaser from 'phaser';
+
 console.log("Game script loaded successfully!");
+
+var config = {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 300,
+  scale: {
+    mode: Phaser.Scale.ZOOM_2X
+  },
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 100 }
+      }
+  },
+  scene: {
+      preload: preload,
+      create: create
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.setBaseURL('/');
+
+  this.load.image('sky', 'assets/background.png');
+  this.load.image('logo', 'assets/title.png');
+  this.load.image('red', 'assets/particle.png');
+}
+
+function create ()
+{
+  this.add.image(200, 150, 'sky');
+
+  var particles = this.add.particles('red');
+
+  var emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+  });
+
+  var logo = this.physics.add.image(200, 50, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
